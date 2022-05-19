b5495e74814f58181444a025e8fc1a2e62c9e98a
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-05-19 22:26:53 +0700

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -11,12 +11,13 @@
 
 Member::Member(){};
 
-Member::Member(string username, string password, string fullName, string phone)
+Member::Member(string username, string password, string fullName, string phone, int credit)
 {
     this->username = username;
     this->password = password;
     this->fullName = fullName;
     this->phone = phone;
+    this->credit = credit;
 }
 
 void Member::showMemberInfo()
@@ -27,7 +28,7 @@ void Member::showMemberInfo()
     cout << "- Phone number: " << this->phone << "\n";
     cout << "- Rating: " << Rating::calculateScores(this->ratings) << "\n";
     cout << "- Credits: " << this->credit << "\n";
-    cout << "- Owner of House occupied: " << this->houseOccupied->owner << "\n";
+    cout << "- House occupied: " << this->houseOccupied << "\n";
 }
 
 void Member::showHouseInfo()
@@ -99,7 +100,9 @@ void Member::addHouse()
 
         cout << "Description: ";
         getline(cin, description);
-        this->houseOwned = new House(this, location, description);
+        this->houseOwned = new House(this, location, description,"","",0,0);
+
+        DataHandler::addHouse(houseOwned);
 
         cout << "You have successfully added a house.\n";
     }
@@ -300,6 +303,7 @@ bool Member::acceptRequest(int num)
         this->houseOwned->addOccupier(list.at(num)->sender);
         this->houseOwned->resetDate(list.at(num)->startDate);
         list.at(num)->sender->houseOccupied = this->houseOwned;
+        list.at(num)->sender->requestSentList.clear();
 
         // Check requestList of house to delete overlapped time request
         for (Request *r : list)
@@ -315,20 +319,6 @@ bool Member::acceptRequest(int num)
 
         list.at(num)->sender->credit -= list.at(num)->sender->numDays * this->houseOwned->consummingCredits;
         this->credit += list.at(num)->sender->numDays * this->houseOwned->consummingCredits;
-
-        for (int i = 0; i < list.at(num)->sender->requestSentList.size(); i++)
-        {
-            for (int j = 0; j < list.at(num)->sender->requestSentList.at(i)->house->requestList.size(); j++)
-            {
-
-                if (list.at(num)->sender == list.at(num)->sender->requestSentList.at(i)->house->requestList.at(j)->sender)
-                {
-                    remove(list.at(num)->sender->requestSentList.at(i)->house->requestList.begin(), list.at(num)->sender->requestSentList.at(i)->house->requestList.end(), list.at(num)->sender);
-                }
-            }
-        }
-
-        list.at(num)->sender->requestSentList.clear();
 
         cout << "You have accepted this request\n";
         return true;
